5b5c0acd8539e34807013d8a0b6ad2a9a380ed12
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-07-04 23:14:37 -0500

    hero

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,28 @@ import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the interenet</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et
+            malesuada fames ac. Pretium fusce id velit ut tortor pretium viverra
+            suspendisse potenti. Pulvinar mattis nunc sed blandit libero
+            volutpat sed cras.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
